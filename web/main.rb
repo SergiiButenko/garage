9cc6b26da60cdd0d865484a82eeadc1543982ec3
sergii <butenko2003@ukr.net>
2017-12-03 16:46:49 +0200

    changed flow

--- web/main.rb
+++ web/main.rb
@@ -1,22 +1,24 @@
 require 'sinatra'
 require 'json'
-require "arduino"
+require 'arduino'
 
 set :bind, '0.0.0.0'
 
 ports = `ls /dev`.split("\n").grep(/usb|ACM/i).map{|d| "/dev/#{d}"}
-puts "port: #{ports}"
-
 port = nil
 ports.each do |p|
+  puts "port #{p}"
   if p.include? 'tty'
     port = p
+    puts "PASSED"
     break
   end
+  puts "FAILED"
 end
 
 # exit 1 if p.nil?
 Arduino.new(ports.first).close
+
 board = Arduino.new(ports.first)
 puts "#{board}"
 
@@ -29,28 +31,33 @@ board.output(7,8,9)
 get '/on/:id' do
   puts "ON #{params['id']}!"
   board.setHigh(params['id'].to_i)
-  
-  st = board.getState(params['id'])
-  puts "#{st.to_s}"
+  sleep 2
 
+  st = { :branches => [{:id => 1, :status => board.getState(1)}, {:id => 2, :status => board.getState(2)}, {:id => 3, :status => board.getState(3)}] }.to_json
+  puts st
+  
   content_type :json
-  { :branches => [{:id => 1, :status => board.getState(1)}, {:id => 2, :status => board.getState(2)}, {:id => 3, :status => board.getState(3)}] }.to_json
+  st
 end	
 
 get '/off/:id' do
   puts "OFF #{params['id']}!"
   board.setLow(params['id'].to_i)
-  
-  st = board.getState(params['id'])
-  puts "#{st.to_s}"
+  sleep 2
 
+  st = { :branches => [{:id => 1, :status => board.getState(1)}, {:id => 2, :status => board.getState(2)}, {:id => 3, :status => board.getState(3)}] }.to_json
+  puts st
+  
   content_type :json
-  { :branches => [{:id => 1, :status => board.getState(1)}, {:id => 2, :status => board.getState(2)}, {:id => 3, :status => board.getState(3)}] }.to_json
+  st
 end	
 
 get '/status' do
+  st = { :branches => [{:id => 1, :status => board.getState(1)}, {:id => 2, :status => board.getState(2)}, {:id => 3, :status => board.getState(3)}] }.to_json
+  puts st
+  
   content_type :json
-  { :branches => [{:id => 1, :status => board.getState(1)}, {:id => 2, :status => board.getState(2)}, {:id => 3, :status => board.getState(3)}] }.to_json
+  st
 end
 
 get '/temp' do 
