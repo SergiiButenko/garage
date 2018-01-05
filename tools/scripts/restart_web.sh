65af4cb58f2fb6ab34bd266240c4770f335f4b65
Sergii Butenko <sergii.butenko@globallogic.com>
2018-01-05 18:54:26 +0200

    changed service

--- tools/scripts/restart_web.sh
+++ tools/scripts/restart_web.sh
@@ -3,7 +3,7 @@ git reset --hard
 git pull origin master
 msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 
-cd /var/www/garage/web/services
+cd /var/www/garage/services
 cp -uv * /etc/systemd/system/
 
 systemctl daemon-reload
