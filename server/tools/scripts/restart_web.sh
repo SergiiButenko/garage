71522631c722e03b74d7eaa7a8912be9efd92c0c
sergii <butenko@linux>
2018-01-10 00:21:52 +0200

    services

--- server/tools/scripts/restart_web.sh
+++ server/tools/scripts/restart_web.sh
@@ -1,12 +1,12 @@
-cd /var/www/garage
+cd /var/www2
 git reset --hard
 git pull origin master
 msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 
-cd /var/www/garage/services
+cd /var/www2/server/services
 cp -uv * /etc/systemd/system/
 
 systemctl daemon-reload
-systemctl restart web.service
+systemctl restart garage_server_web.service
 
 echo 'HEAD is now '$msg
